e0ff21be0185da30a2f014c74a6683baf7121de9
aniruddhnishad <mr.aniruddhnishad@gmail.com>
2025-08-25 00:58:40 +0530

    fffh

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,13 @@ import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { prettyJSON } from 'hono/pretty-json'
 import { secureHeaders } from 'hono/secure-headers'
-import { compress } from 'hono/compress'
 
 import appConfig from './config/appConfig.js'
 
 const PORT: number = appConfig.PORT
 
 const app = new Hono()
-app.use(cors(), prettyJSON(), secureHeaders(), compress())
+app.use(cors(), prettyJSON(), secureHeaders())
 
 app.get('/', (c) => {
   // const env1 = env(c)
