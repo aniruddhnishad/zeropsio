e5200b9b3b45a17e2de875b2e5fd30fb53d83267
aniruddhnishad <mr.aniruddhnishad@gmail.com>
2025-08-25 00:56:25 +0530

    fffh

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ import { cors } from 'hono/cors'
 import { prettyJSON } from 'hono/pretty-json'
 import { secureHeaders } from 'hono/secure-headers'
 import { compress } from 'hono/compress'
-import * as dotenv from 'dotenv'
-dotenv.config();
 
-const PORT: number = Number(process.env.PORT) || 3000
+import appConfig from './config/appConfig.js'
+
+const PORT: number = appConfig.PORT
 
 const app = new Hono()
 app.use(cors(), prettyJSON(), secureHeaders(), compress())
